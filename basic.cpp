bb77b9efed3f6496d9609398a97b2c7259467d1e
Edy Syafrianto <117509159+edy2209@users.noreply.github.com>
2022-11-18 21:35:58 -0800

    basic dan contoh program dari c++
    selamat belajar

---
+++ basic.cpp
@@ -0,0 +1,18 @@
+//cara menulis coment pada c++ dengan tanda slas // 2 x
+//selanjutnya struktur pada c++
+//include iostream" struktur dasar pada c++", stdio.h,match.h yang bisa di pelajari di google
+//program inputan dasar bisa dengan cara berikut
+#include <iostream>
+using namespace std;
+
+main(){
+	int panjang,lebar,keliling; //deklarasi type data
+	cout<<"merupakan program inputan"<<endl; //ini merupakan program print /tampilan
+	cout<<"masukan panjang : " ;cin >>panjang; //program inputan
+	cout<<"masukan lebar :  " ;cin >>lebar;
+	//program aritamtika juga sama saja penambahb=+,kali=*,kurang=-,bagi=/ dan lain sebgainya
+	//contoh hitung luas persegi panjang
+	keliling=panjang*lebar;
+	cout<<"maka keliling bangunan persegi panjang adalah : "<<keliling<< " cm"<<endl;
+	
+}
